1ef892a3ef46b8c669135094a2657e2d2d4d252d
김희성 <khsung0@nate.com>
2022-05-20 09:17:27 +0900

    로그인 정보 담기

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,14 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    logined: {
+      userId: "",
+      username: "",
+      email: "",
+      joindate: "",
+    },
+  },
   getters: {},
   mutations: {},
   actions: {},
